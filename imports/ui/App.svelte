37b74b69c8a080344d64b46d264341093d8b004c
Aaron Rosenzweig <info@kokokino.com>
2025-10-12 12:36:28 -0400

    6.5: Pending tasks

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -5,6 +5,7 @@
   import Task from "./Task.svelte";
 
   let newTask = '';
+  let hideCompleted = false;
 
   async function addTask(event) {
     event.preventDefault();
@@ -17,16 +18,29 @@
     }
   }
 
+  function toggleHideCompleted() {
+    hideCompleted = !hideCompleted;
+  }
+
   $m: handle = Meteor.subscribe("tasks");
   $m: subIsReady = handle.ready();
-  $m: tasks = TasksCollection.find({}, { sort: { createdAt: -1, _id: -1 } }).fetch();
+
+  // Reactive tasks with filter
+  $m: tasks = TasksCollection.find(
+    hideCompleted ? { isChecked: { $ne: true } } : {},
+    { sort: { createdAt: -1, _id: -1 } }
+  ).fetch();
+
+  // Reactive incomplete count
+  $m: incompleteCount = TasksCollection.find({ isChecked: { $ne: true } }).count();
+  $m: incompleteDisplay = incompleteCount > 0 ? `(${incompleteCount})` : '';
 </script>
 
 <div class="app">
   <header>
     <div class="app-bar">
       <div class="app-header">
-        <h1>📝️ Todo List</h1>                    
+        <h1>📝️ To Do List {incompleteDisplay}</h1>
       </div>
     </div>
   </header>
@@ -36,6 +50,16 @@
       <input type="text" placeholder="Type to add new tasks" bind:value={newTask} />
       <button type="submit">Add Task</button>
     </form>
+
+    <div class="filter">
+      <button on:click={toggleHideCompleted}>
+        {#if hideCompleted}
+          Show All
+        {:else}
+          Hide Completed
+        {/if}
+      </button>
+    </div>
 
     <ul class="tasks">
       {#if subIsReady}
